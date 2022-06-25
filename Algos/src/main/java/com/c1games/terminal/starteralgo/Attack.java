6f2091b5155a42df7cfe805d768b6ed3d0afeac0
Ca7Ac1 <Ca7Ac1@gmail.com>
2022-06-24 21:38:01 -0400

    Add attack file todo

---
+++ Algos/src/main/java/com/c1games/terminal/starteralgo/Attack.java
@@ -0,0 +1,20 @@
+package com.c1games.terminal.starteralgo;
+
+import com.c1games.terminal.algo.*;
+import com.c1games.terminal.algo.io.GameLoop;
+import com.c1games.terminal.algo.io.GameLoopDriver;
+import com.c1games.terminal.algo.map.GameState;
+import com.c1games.terminal.algo.map.MapBounds;
+import com.c1games.terminal.algo.map.Unit;
+import com.c1games.terminal.algo.units.UnitType;
+
+import java.util.*;
+
+public class Attack {
+
+    private GameState state;
+
+    public Attack(GameState state) {
+        this.state = state;
+    }
+}
