f85597f17d999c12bbee5923d65d2a4a78270b8e
Ca7Ac1 <Ca7Ac1@gmail.com>
2022-06-24 21:43:33 -0400

    move around function to attack

--- Algos/src/main/java/com/c1games/terminal/starteralgo/Attack.java
+++ Algos/src/main/java/com/c1games/terminal/starteralgo/Attack.java
@@ -17,4 +17,32 @@ public class Attack {
     public Attack(GameState state) {
         this.state = state;
     }
+
+    //TODO: Account for shields gained from supports
+    private Coords leastDamageSpawnLocation(GameState move, List<Coords> locations) {
+        List<Float> damages = new ArrayList<>();
+
+        for (Coords location : locations) {
+            List<Coords> path = move.pathfind(location, MapBounds.getEdgeFromStart(location));
+            float totalDamage = 0;
+            for (Coords dmgLoc : path) {
+                List<Unit> attackers = move.getAttackers(dmgLoc);
+                for (Unit unit : attackers) {
+                    totalDamage += unit.unitInformation.attackDamageWalker.orElse(0);
+                }
+            }
+            GameIO.debug().println("Got dmg:" + totalDamage + " for " + location);
+            damages.add(totalDamage);
+        }
+
+        int minIndex = 0;
+        float minDamage = 9999999;
+        for (int i = 0; i < damages.size(); i++) {
+            if (damages.get(i) <= minDamage) {
+                minDamage = damages.get(i);
+                minIndex = i;
+            }
+        }
+        return locations.get(minIndex);
+    }
 }
